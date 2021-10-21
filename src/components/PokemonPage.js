9754742d7cf7749d1037885ee1e5f91de5ea4267
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-10-21 15:16:08 -0700

    initial commit

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,6 +5,23 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+    state = {
+      pokemons: [],
+    }
+
+
+    componentDidMount() {
+      fetch("http://localhost:3000/pokemon")
+      .then(resp => resp.json())
+      .then(pokemons => {
+          // console.log(pokemons)
+          this.setState({pokemons})
+          // console.log(pokemons)
+      })
+    }
+
+
   render() {
     return (
       <Container>
@@ -14,7 +31,7 @@ class PokemonPage extends React.Component {
         <br />
         <Search />
         <br />
-        <PokemonCollection />
+        <PokemonCollection  pokemons={this.state.pokemons}/>
       </Container>
     )
   }
