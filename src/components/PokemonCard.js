002d0584c4d1afd7d0aed0e824c032605e06c112
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-10-21 15:57:00 -0700

    displayed pokemon

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+    state = {
+      clicked: false, 
+      image: this.props.pokemon.sprites.back
+    }
+
+
+    handleToggle = () =>{
+      if(this.state.clicked === false) {
+        this.setState({clicked: true, image: this.props.pokemon.sprites.front})
+      } else {
+        this.setState({clicked: false, image: this.props.pokemon.sprites.back})
+      }
+      
+    }
+ 
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.handleToggle}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.image} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,21 +1,15 @@
 import React from 'react'
-// import PokemonCard from './PokemonCard'
+import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
 
-  componentDidMount() {
-   
-    // debugger;
-  }
-
-
   render() {
-    console.log(this.props)
+    // console.log(this.props)
+    let renderPokemons = this.props.pokemons.map((pokemon) => <PokemonCard key={pokemon.id} pokemon={pokemon}/>)
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-        
+        {renderPokemons}
       </Card.Group>
     )
   }
